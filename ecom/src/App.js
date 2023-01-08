419e56126831ec60c4bfbececfbd99785440fa30
Nominest <nominest3@gmail.com>
2023-01-08 22:40:36 +0800

    data

--- ecom/src/App.js
+++ ecom/src/App.js
@@ -1,6 +1,5 @@
 import logo from "./logo.svg";
 import "./App.css";
-import  {data} from './util/Data';
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import Main from "./components/Main.jsx";
